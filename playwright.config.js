ca637ef4bc1e9ab7e62610306b75ff0bf4ab213e
Simon Praetorius <moin@praetorius.me>
2026-06-08 19:25:37 +0200

    Initial commit

---
+++ playwright.config.js
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "tests",
+  fullyParallel: true,
+  retries: 0,
+  reporter: [
+    ["dot"],
+    ["@praetorius/visual-regression-tester/reporter"],
+  ],
+  use: {
+    ignoreHTTPSErrors: true,
+  },
+  globalSetup: ["@praetorius/visual-regression-tester/collector"],
+  projects: [
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+  ],
+});
---
+++ visualregression.config.js
@@ -0,0 +1,7 @@
+// @ts-check
+import { defineConfig } from "@praetorius/visual-regression-tester";
+
+export default defineConfig({
+  referenceUrl: "https://www.example.com",
+  subjectUrl: "https://example.ddev.site",
+});
