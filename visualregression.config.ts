757496b12eae57199f95414ee145db1ebb569e16
Simon Praetorius <simon@praetorius.me>
2026-06-22 14:48:04 +0200

    [TASK] Clarify default config options

--- visualregression.config.ts
+++ visualregression.config.ts
@@ -3,4 +3,14 @@ import { defineConfig } from "@praetorius/visual-regression-tester";
 export default defineConfig({
   referenceUrl: "https://www.example.com",
   subjectUrl: "https://example.ddev.site",
+  diff: {
+    threshold: 0.2,
+    maxPixelsDifferent: 10,
+    maxPercentDifferent: 0,
+  },
+  run: {
+    limit: -1,
+    status: ['scheduled', 'passed', 'failed'],
+    skipAccepted: true,
+  },
 });
